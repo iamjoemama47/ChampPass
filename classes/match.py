f2baabccfe96d3d92ac8cea8f5ef9a7b01429989
Ares-DM <ares.demey08@gmail.com>
2026-03-19 11:06:03 +0100

    classe match en ticket

---
+++ classes/match.py
@@ -0,0 +1,23 @@
+class match ():
+    def __init__(self,match_ID=None,beginUur=None,eindUur=None,ploeg_1=None,ploeg_2=None,stadion_ID=None,score_ploeg_1=None,score_ploeg_2=None):
+        self.match_ID=match_ID
+        self.beginUur=beginUur
+        self.eindUur=eindUur
+        self.ploeg_1=ploeg_1
+        self.ploeg_2=ploeg_2
+        self.stadion_ID=stadion_ID
+        self.score_ploeg_1=score_ploeg_1
+        self.score_ploeg_2=score_ploeg_2
+    
+    def start ():
+        pass
+    
+    def einde():
+        pass
+    
+    def aflassen():
+        pass
+    
+    def lijstMatchen ():
+        pass
+        
--- classes/ticket.py
+++ classes/ticket.py
@@ -1,7 +1,9 @@
-def ticket(self,prijs,klant_ID,match_ID):
-    self.prijs = prijs
-    self.klant_ID = klant_ID
-    self.match_ID = match_ID
+class ticket():
+    def __init__(self,ticket_id=None,prijs=None,klant_ID=None,match_ID=None):
+        self.ticket_id=ticket_id
+        self.prijs = prijs
+        self.klant_ID = klant_ID
+        self.match_ID = match_ID
     
     def kopen ():
         pass
