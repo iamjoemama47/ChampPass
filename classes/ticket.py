b9edc0be71ab4c6d50cd5b2529b49dd48e751d8f
Ares-DM <ares.demey08@gmail.com>
2026-03-24 10:45:20 +0100

    aantal_ticket

--- classes/ticket.py
+++ classes/ticket.py
@@ -1,9 +1,10 @@
 class ticket():
-    def __init__(self,ticket_id=None,prijs=None,klant_ID=None,match_ID=None):
+    def __init__(self,ticket_id=None,prijs=None,klant_ID=None,match_ID=None, aantal_ticket =None):
         self.ticket_id=ticket_id
         self.prijs = prijs
         self.klant_ID = klant_ID
         self.match_ID = match_ID
+        self.aantal_ticket=aantal_ticket
     
     def kopen ():
         pass
