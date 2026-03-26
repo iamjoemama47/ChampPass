737e73496946e760e106591ead6b058ae7f732a4
Pol-De-Mol <jolandecoster@gmail.com>
2026-03-26 12:01:47 +0100

    test update bijna volledig

--- test_stadion.py
+++ test_stadion.py
@@ -5,12 +5,17 @@ from classes.stadion import *
 import classes.dbconfig as db
 
 mydb = db.Connect()
-
+#test read
 myStadion = Stadion.read(mydb,"Maricolen Stadion")
 print(myStadion)
 
 #myStadion = Stadion.create (mydb, "3", "Eeklo Stadion", "1000")
 #print (myStadion)
 
+#test lijst
 myStadion = Stadion.lijst_stadion(mydb)
+print(myStadion)
+
+#test update
+myStadion = Stadion.update(mydb, "25", "Maricolen_st", "2")
 print(myStadion)
