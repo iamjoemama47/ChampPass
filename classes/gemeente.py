47a8ee7d31d62456ed971d5b4be1cad1157d5157
Ares-DM <ares.demey08@gmail.com>
2026-03-26 11:42:16 +0100

    gemeente+test

--- classes/gemeente.py
+++ classes/gemeente.py
@@ -3,34 +3,36 @@ from classes.dbconfig import Connect
 
 
 class Gemeente:
-    def __init__(self, gemeente_ID=None, gemeente_naam=None, post_code=None,land_code=None):
+    def __init__(self, gemeente_ID=None, gemeente_naam=None, postcode=None,land_Code=None):
         self.gemeente_ID = gemeente_ID
         self.gemeente_naam = gemeente_naam
-        self.post_code = post_code
-        self.land_code = land_code
+        self.postcode = postcode
+        self.land_Code = land_Code
     
     def __repr__(self):
-        return f'gemeente(gemeente_ID={self.gemeente_ID},gemeente_naam={self.gemeente_naam}, post_code{self.post_code}, land_code={self.land_code})'
+        return f'gemeente(gemeente_ID={self.gemeente_ID},gemeente_naam={self.gemeente_naam}, post_code{self.postcode}, land_code={self.land_Code})'
 
-    def createGemeente(self,the_db):
-        sqltxt = "INSERT INTO gemeente (gemeenteNaam, postcode, landCode) VALUES (%s, %s, %s)"
+    def createGemeente(the_db, gemeente_naam, postcode, land_Code):
+        sqltxt = "INSERT INTO gemeente (gemeente_naam, postcode, land_Code) VALUES (%s, %s, %s)"
         mycursor = the_db.cursor()
-        mycursor.execute(sqltxt(self.gemeenteNaam, self.postcode, self.landCode))
+        mycursor.execute(sqltxt, (gemeente_naam, postcode, land_Code))
+        # moet nog verder gewerkt worden: the_db.commit()
         mycursor.close()
 
-    def readGemeente(the_db, gemeenteNaam):
-        sqltxt =  "SELECT gemeenteNaam, postcoden, landCode FROM gemeente where gemeenteNaam like %s"
+
+    def readGemeente(the_db, gemeente_naam):
+        sqltxt =  "Select gemeente_ID, gemeente_naam, postcode, land_Code FROM gemeente where gemeente_naam like %s"
         mycursor = the_db.cursor()
-        mycursor.execute(sqltxt,(gemeenteNaam,))
+        mycursor.execute(sqltxt,(gemeente_naam,))
         result = mycursor.fetchone()
         mycursor.close()
         if result:
-            Gemeente(*result)
+            return Gemeente(*result)
         else:
             return None
         
     def lijst_gemeentes(the_db):
-        sqltxt = "SELECT gemeente_ID, gemeenteNaam, postcode, landCode"
+        sqltxt = "SELECT gemeente_ID, gemeente_naam, postcode, land_Code FROM gemeente"
         mycursor = the_db.cursor()
         mycursor.execute(sqltxt)
         result = mycursor.fetchall()
--- test-Gemeente.py
+++ test-Gemeente.py
@@ -1,6 +1,28 @@
+import os
+import sys
+
+path,_ = os.path.split(os.path.realpath(__file__))
+path = os.path.join(path, "myvenv/Lib/site-packages")
+if not path in sys.path:
+    sys.path.append(path)
+
+
+
+import classes.dbconfig as db
 import mysql.connector
-from classes.dbconfig import Connect
+from classes.gebruiker import Gebruiker
+
 from classes.gemeente import *
 
 
-mydb =
+mydb = db.Connect()
+
+myGemeente = Gemeente.readGemeente(mydb,'Maldegem')
+print (myGemeente)
+
+myGemeente = Gemeente.lijst_gemeentes(mydb)
+print(myGemeente)
+
+myGemeente = Gemeente.createGemeente(mydb, 'Nieuwpoort', '8400', 'BEL')
+print(myGemeente)
+
