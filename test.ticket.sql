acce9a054d54488392840849b26ee9d80d35f311
Ares-DM <ares.demey08@gmail.com>
2026-04-21 10:03:27 +0200

    ticket

--- test.ticket.sql
+++ test.ticket.sql
@@ -7,3 +7,5 @@ UPDATE ticket set `ticket_ID`=1,`prijs`=50,`klant_ID`=1, `match_ID`=1 WHERE `tic
 INSERT into ticket(`ticket_ID`,`prijs`, `klant_ID`,`match_ID`)VALUES(1,50,1,1)
 
 DELETE from ticket WHERE `ticket_ID`=1
+
+select*from stadion
---
+++ test_ticket.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+path,_ = os.path.split(os.path.realpath(__file__))
+path = os.path.join(path, "myvenv/Lib/site-packages")
+if not path in sys.path:
+    sys.path.append(path)
+
+
+
+import classes.dbconfig as db
+import mysql.connector
+from classes.ticket import Ticket
+
+from classes.ticket import *
+
+
+mydb = db.Connect()
+
+
+
+
+#update 
+myTicket=Ticket.createTicket(mydb,"1","40","2","1")
+print(myTicket)
+
+
