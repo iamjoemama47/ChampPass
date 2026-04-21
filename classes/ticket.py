58277894e01343199126c38a5dc89dba8e9034cf
Ares-DM <ares.demey08@gmail.com>
2026-04-21 09:32:33 +0200

    ticket en test

--- classes/ticket.py
+++ classes/ticket.py
@@ -1,14 +1,33 @@
 class Ticket():
-    def __init__(self,ticket_id=None,prijs=None,klant_ID=None,match_ID=None, aantal_ticket =None):
-        self.ticket_id=ticket_id
+    def __init__(self,ticket_ID=None,prijs=None,klant_ID=None,match_ID=None, aantal_ticket =None):
+        self.ticket_ID=ticket_ID
         self.prijs = prijs
         self.klant_ID = klant_ID
         self.match_ID = match_ID
         self.aantal_ticket=aantal_ticket
     def __repr__(self):
-        return f'ticket(ticket_id={self.ticket_id},prijs={self.prijs}, klant_ID={self.klant_ID}, match_ID={self.match_ID}, aantal_ticket={self.aantal_ticket})'
+        return f'ticket(ticket_ID={self.ticket_ID},prijs={self.prijs}, klant_ID={self.klant_ID}, match_ID={self.match_ID}, aantal_ticket={self.aantal_ticket})'
     
     def kopen ():
         pass
     def betalen():
         pass
+    
+    def createTicket(self,the_db):
+        sqltxt="insert into ticket(ticket_ID, prijs, klant_ID,match_ID)Values(%s,%s,%s,%s)"
+        mycursor=the_db.cursour()
+        mycursor.execute(sqltxt,(self.ticket_ID,self.prijs,self.klant_ID,self.match_ID))
+        mycursor.close()
+
+
+    def updateTicket(self,the_db):
+        sqltxt="UPDATE ticket set ticket_ID=%s,prijs=%s,klant_ID=%s,match_ID=%s WHERE ticket_ID=%s"
+        mycursor=the_db.cursour()
+        mycursor.execute(sqltxt,(self.ticket_ID,self.prijs,self.klant_ID,self.match_ID))
+        mycursor.close()
+
+    def deleteTicket(the_db,ticket_ID):
+        sqltxt="DELETE from ticket WHERE ticket_ID=%s"
+        mycursor=the_db.cursor()
+        mycursor.execute(sqltxt,(ticket_ID,))
+        mycursor.close()
---
+++ test.ticket.sql
@@ -0,0 +1,9 @@
+select*from ticket
+
+INSERT into ticket(`ticket_ID`,`prijs`,`klant_ID`,`match_ID`)VALUES(3,30,2,2)
+
+UPDATE ticket set `ticket_ID`=1,`prijs`=50,`klant_ID`=1, `match_ID`=1 WHERE `ticket_ID`=1
+
+INSERT into ticket(`ticket_ID`,`prijs`, `klant_ID`,`match_ID`)VALUES(1,50,1,1)
+
+DELETE from ticket WHERE `ticket_ID`=1
