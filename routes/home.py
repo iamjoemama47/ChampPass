c18390ae25305bdca6565167e5ff158259e270f7
JXPP£ <welvaert.joppe@gmail.com>
2026-05-12 08:56:43 +0200

    html

--- routes/home.py
+++ routes/home.py
@@ -5,11 +5,11 @@ home_bp = Blueprint('home', __name__)
 @home_bp.route('/')
 def home():
     if 'login' not in session:  # check of sessie-variabele login bestaat
-        return render_template('login.html')
+        return render_template('index.html')
     if session['login'] == "":  #er is nog geen login gebeurd
                 return render_template('login.html',the_comment="Je moet eerst inloggen")    
     #einde check login
 
     #hier komt de code voor de homepage
     pass
-    return render_template('home.html')
+    return render_template('index.html')
