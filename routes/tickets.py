7846291d4cf4281dd180dd4bc887c6f7a3d6d53f
Ares-DM <ares.demey08@gmail.com>
2026-05-19 10:41:46 +0200

    Ares-0519-01

--- routes/tickets.py
+++ routes/tickets.py
@@ -2,19 +2,19 @@ from flask import Blueprint, render_template
 
 import classes.dbconfig as db
 import mysql.connector
-from classes.ticket import Ticket
+from classes.wedstrijd import Wedstrijd
 
 
 tickets_bp = Blueprint('tickets', __name__)
 mydb = db.Connect()
 
-@tickets_bp.route('/tickets/<int:ticket_id>')
-def ticketdetail(ticket_id):
+@tickets_bp.route('/tickets/<int:wedstrijd_id>')
+def tickets(wedstrijd_ID):
 
     try:
-        myticket=Ticket.get_by_id(mydb,ticket_id)
+        myticket=Wedstrijd.get_by_id(mydb,wedstrijd_ID)
     except mysql.connector.Error as e:
-        print(f"Fout bij ophalen van tickets: {e}")
+        print(f"Fout bij ophalen van wedstrijden: {e}")
         myticket = []
 
-    return render_template('ticketdetail.html',ticket = myticket)
+    return render_template('tickets.html',ticket = myticket)
