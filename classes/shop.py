bc5927c909749243520bd096f627cb3247247fe3
Pol-De-Mol <jolandecoster@gmail.com>
2026-05-21 10:37:02 +0200

    shop

---
+++ classes/shop.py
@@ -0,0 +1,16 @@
+import mysql.connector
+from classes.dbconfig import Connect
+
+class Shop:
+    def __init__(self, ticket_ID=None, prijs=None, klant_ID=None, match_ID=None, aantal_tickets=None, betaald=None):
+        self.ticket_ID = ticket_ID
+        self.prijs = prijs
+        self.klant_ID = klant_ID
+        self.match_ID = match_ID
+        self.aantal_tickets = aantal_tickets
+        self.betaald = betaald
+    
+    def __repr__(self):
+        return f'Shop (ticket_ID={self.ticket_ID}, prijs={self.prijs}, klant_ID={self.klant_ID}, match_ID={self.match_ID}, aantal_tickets={self.aantal_tickets}, betaald={self.betaald})'
+    
+    
